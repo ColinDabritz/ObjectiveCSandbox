20e26d2d7ac5ef4f0cf0f618e60e13735b821806
Colin Dabritz <colin@colin-dabritzs-macbook-pro-4.local>
2011-08-11 14:01:54 -0700

    Simple "Guess" example, dusting off the C skills.

--- Sandbox/Sandbox/main.m
+++ Sandbox/Sandbox/main.m
@@ -7,12 +7,40 @@
 //
 
 #import <Foundation/Foundation.h>
+#include <stdlib.h>
 
 int main (int argc, const char * argv[])
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    NSLog(@"Hello, World!");
+    // simple example of the 'guess' program
+    // the program chooses a target number 1-100
+    // the user enters integer guesses
+    // the program gives feedback if the guess is low or high, until the user guesses right
+
+    // initialize the setup, and choose first target
+    int target = arc4random() % 100 + 1;
+    int guess;
+    
+    printf("I choose a number between 1 and 100. Would you care to guess it?\n");
+
+    // get guesses from the user and respond
+    while(scanf("%d", &guess))
+    {
+        if(guess > target) // guess is high
+        {
+            printf("Too high. Guess again?\n");
+        }
+        else if(guess < target) // guess is low
+        {
+            printf("Too low. Guess again?\n");
+        }
+        else if(guess == target) // completed one game. Start another.
+        {
+            printf("Correct! I've chosen a new number bewteen 1 and 100. Would you care to guess it?\n");
+            target = arc4random() % 100 + 1;
+        }
+    }
 
     [pool drain];
     return 0;
